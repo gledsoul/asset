5842c3c98a5db550bb786e8770aadf5bca06bbec
Gledson Joaquim Lopes <gledsonjoaquimlopes@Gledsons-MacBook-Pro.local>
2023-04-17 19:11:59 +0100

    node

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import logo from "./logo.svg";
+import "./MintToken";
+import "./App.css";
+import MintToken from "./MintToken";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MintToken />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
